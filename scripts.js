b5d29c253c81ed82d8a289cc52283234a383046e
Mischka van Wyk <m.vanwyk@lab.clicklearning.org>
2023-08-02 21:25:11 +0200

    added changes to challenge

--- scripts.js
+++ scripts.js
@@ -1,11 +1,31 @@
 // scripts.js
 
+
+/**
+ * @param {number} dividend - number to be divided
+ * @param {number} divider - number to be divided by
+ */
+
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
-
+const body = document.querySelector("[data-body]");
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+    event.preventDefault();
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+    result.innerText = dividend / divider;
+
+    if (!dividend || !divider) {
+        result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    }
+    else if (dividend < 0 || divider < 0) {
+        result.innerText = "Division not performed. Invalid number provided. Try again";
+    }
+    else if (isNaN(dividend) || isNaN(divider)) {
+        body.innerText = "Something critical went wrong. Please reload the page";
+        console.error(body.innerText);
+    }
+    else {
+        result.innerText = Math.floor(dividend / divider);
+    }
 });
