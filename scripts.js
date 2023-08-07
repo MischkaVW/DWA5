f7a7d631677b740197a0a197999679d507eac97b
Mischka van Wyk <m.vanwyk@lab.clicklearning.org>
2023-08-07 20:16:09 +0200

    corrections made to challenge

--- scripts.js
+++ scripts.js
@@ -8,7 +8,6 @@
 
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
-const body = document.querySelector("[data-body]");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const entries = new FormData(event.target);
@@ -22,8 +21,8 @@ form.addEventListener("submit", (event) => {
         result.innerText = "Division not performed. Invalid number provided. Try again";
     }
     else if (isNaN(dividend) || isNaN(divider)) {
-        body.innerText = "Something critical went wrong. Please reload the page";
-        console.error(body.innerText);
+        result.innerText = "Something critical went wrong. Please reload the page";
+        console.error("Something critical went wrong. Please reload the page");
     }
     else {
         result.innerText = Math.floor(dividend / divider);
